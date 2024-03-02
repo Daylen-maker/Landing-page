110d488b0dee0f76a7020896ef4159b466acc473
Vytautas <vytas.lego@gmail.com>
2024-03-02 11:13:50 +0200

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header className='bg-black text-emerald-500 text-center text-3xl p-4'>Welcome to Tailwind CSS with React</header>
     </div>
   );
 }
